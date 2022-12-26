53134ef67fc08faf623b8c642b46516dbb809145
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2022-12-26 21:33:11 +0530

    Time: 30 ms (79.70%), Space: 31.2 MB (17.72%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,78 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int prefix_sum  =0;
+        for(int i= 0; i < nums.size(); i++){
+            prefix_sum += nums[i];
+        }
+        int left_sum =0; 
+        int right_sum = prefix_sum ;
+        for(int i= 0; i < nums.size(); i++){
+            right_sum = right_sum - nums[i];
+            if(left_sum == right_sum)
+                return i;
+            
+            left_sum += nums[i];
+        }
+        return -1;
+    }
+};
+
+// Same Approach
+// ----------------
+
+// int pivotIndex(vector<int>& nums) {
+//         int sumR = accumulate(nums.begin(), nums.end(), 0);
+//         int sumL = 0;
+//         for (int i = 0; i < nums.size(); i++) {
+//              sumR -= nums[i];
+//              if (sumL == sumR)
+//                  return i;
+//              sumL += nums[i];
+//          }
+//          return -1;
+//      }
+//  };
+
+
+
+
+// Time Limit Exceeded
+// -------------------
+
+// int pivotIndex(vector<int>& nums) {
+//         int suml=0,sumr=0,j=0,k=0;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             suml=0;
+//             sumr=0;
+//             j=i+1;
+//             k=i-1;
+//             while(j<nums.size())
+//             {
+//                 if(i==nums.size()-1)
+//                 {
+//                     sumr=0;
+//                     break;
+//                 }
+//                 sumr+=nums[j];
+//                 j++;
+//             }
+//             while(k>=0)
+//             {
+//                 if(i==0)
+//                 {
+//                     suml=0;
+//                     break;
+//                 }
+//                 suml+=nums[k];
+//                 k--;
+//             }
+//             if(suml==sumr)
+//             {
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
