8ce1f159cf80e1dbf86feff57834a7bda52deed4
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2022-12-26 11:45:57 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (50.18%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        if(num==0)
+        {
+            return 1;
+        }
+        int m=num;
+        int mask=0;
+        while(m)
+        {
+            mask=(mask<<1)|1;
+            m=m>>1;
+        }
+        int ans=(~num)&mask;
+        return ans;
+    }
+};
