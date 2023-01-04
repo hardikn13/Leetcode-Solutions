3e1d7f88799ab71caf3cc75c1763be4d76af1b07
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2023-01-05 00:33:56 +0530

    Time: 7 ms (45.99%), Space: 8 MB (36.61%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> s;
+        for(int i=1;i<=n;i++)
+        {
+            if(i%3==0&&i%5==0)
+            {
+                s.emplace_back("FizzBuzz");
+            }
+            else if(i%3==0&&i%5!=0)
+            {
+                s.emplace_back("Fizz");
+            }
+            else if(i%5==0&&i%3!=0)
+            {
+                s.emplace_back("Buzz");
+            }
+            else
+            {
+                s.emplace_back(std::to_string(i));
+            }
+        }
+        return s;
+    }
+};
