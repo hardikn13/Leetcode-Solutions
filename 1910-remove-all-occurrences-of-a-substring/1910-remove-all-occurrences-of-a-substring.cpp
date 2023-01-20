6646766b32c010d2e5c0e407ddfb466e3ea8ddfc
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2023-01-20 08:20:37 +0530

    Time: 3 ms (89.91%), Space: 6.8 MB (19.69%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -6,5 +6,23 @@ public:
             s.erase(s.find(part),part.size());
         }
         return s;
+    
+    
+    
+	// string x = s;
+	// int n = s.size(), m = part.size(), i, j;
+	// for (i = 0, j = 0; i < n; i++) {
+	// 	x[j++] = s[i];
+	// 	if (j >= m && x.substr(j - m, m) == part)
+	// 		j -= m;
+	// }
+	// return x.substr(0, j);
+    
+    
+    
+    
+    
+    
+    
     }
 };
