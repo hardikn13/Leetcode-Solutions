22a7baf7e5b1312a7abbaf6075517327a668e030
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2023-07-03 01:07:45 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (64.56%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+        vector<int> prev(n, 0);
         for(int i = 0; i < m; i++)
         {
+            vector<int> curr(n, 0);
             for(int j = 0; j < n; j++)
             {
                 if(i == 0 && j == 0)
                 {
-                    dp[i][j] = 1;
+                    curr[j] = 1;
                 }
                 else
                 {
@@ -16,16 +17,52 @@ public:
                     int left = 0;
                     if(i > 0)
                     {
-                        up = dp[i-1][j];
+                        up = prev[j];
                     }
                     if(j > 0)
                     {
-                        left = dp[i][j-1];
+                        left = curr[j-1];
                     }
-                    dp[i][j] = up + left;
+                    curr[j] = up + left;
                 }
             }
+            prev = curr;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
+
+
+// Tabulation
+// ----------
+// // class Solution {
+// public:
+//     int uniquePaths(int m, int n) {
+//         vector<vector<int>> dp(m, vector<int>(n, -1));
+//         for(int i = 0; i < m; i++)
+//         {
+//             for(int j = 0; j < n; j++)
+//             {
+//                 if(i == 0 && j == 0)
+//                 {
+//                     dp[i][j] = 1;
+//                 }
+//                 else
+//                 {
+//                     int up = 0;
+//                     int left = 0;
+//                     if(i > 0)
+//                     {
+//                         up = dp[i-1][j];
+//                     }
+//                     if(j > 0)
+//                     {
+//                         left = dp[i][j-1];
+//                     }
+//                     dp[i][j] = up + left;
+//                 }
+//             }
+//         }
+//         return dp[m-1][n-1];
+//     }
+// };
