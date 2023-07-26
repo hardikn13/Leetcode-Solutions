86fcb6d0084474e295da15657ef5d16acb4c7816
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2023-07-26 19:20:45 +0530

    Time: 4 ms (40.40%), Space: 6.4 MB (36.07%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack <char> stk;
+        if(s.size() == 1)
+        {
+            return 0;
+        }
+        int z = 0;
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{')
+            {
+                stk.push(s[i]);
+                z++;
+            }
+            else if(s[i] == ')' || s[i] == ']' || s[i] == '}')
+            {
+                if(z == 0)
+                {
+                    return 0;
+                }
+                if(s[i] == ')' && stk.top() == '(' && stk.empty() == 0)
+                {
+                    stk.pop();
+                    z--;
+                }
+                else if(s[i] == ']' && stk.top() == '[' && stk.empty() == 0)
+                {
+                    stk.pop();
+                    z--;
+                }
+                else if(s[i] == '}' && stk.top() == '{' && stk.empty() == 0)
+                {
+                    stk.pop();
+                    z--;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+        if(stk.empty() == 0)
+        {
+            return 0;
+        }
+        return 1;
+    }
+};
