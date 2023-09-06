eb73796b688cedc25ce7569b7476d52a0e9ed456
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2023-09-06 11:35:39 +0530

    Time: 385 ms (5.02%), Space: 55.9 MB (5.02%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -4,16 +4,17 @@ public:
     int cherryPickup(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, 0)));
+        vector<vector<int>> front(m, vector<int>(m, 0));
+        vector<vector<int>> curr(m, vector<int>(m, 0));
         
         for(int j1 = 0; j1 < m; j1++)
         {
             for(int j2 = 0; j2 < m; j2++)
             {
                 if(j1 == j2)
-                    dp[n - 1][j1][j2] = grid[n - 1][j1];
+                    front[j1][j2] = grid[n - 1][j1];
                 else
-                    dp[n - 1][j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
+                    front[j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
             }
         }
         
@@ -38,21 +39,79 @@ public:
                                 value = grid[i][j2] + grid[i][j1];
                             
                             if(j1 + r1 >= 0 && j1 + r1 < m && j2 + r2 >= 0 && j2 + r2 < m)
-                                value += dp[i + 1][j1 + r1][j2 + r2];
+                                value += front[j1 + r1][j2 + r2];
                             else
                                 value = -1e9;
                             
                             maxi = max(maxi,value);
                         }
                     }
-                    dp[i][j1][j2] = maxi;
+                    curr[j1][j2] = maxi;
                 }
             }
+            front = curr;
         }
-        return dp[0][0][m - 1];
+        return front[0][m - 1];
     }
 };
 
+
+
+
+
+// TABULATION
+// ----------
+
+//     int cherryPickup(vector<vector<int>>& grid) {
+//         int n = grid.size();
+//         int m = grid[0].size();
+//         vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, 0)));
+        
+//         for(int j1 = 0; j1 < m; j1++)
+//         {
+//             for(int j2 = 0; j2 < m; j2++)
+//             {
+//                 if(j1 == j2)
+//                     dp[n - 1][j1][j2] = grid[n - 1][j1];
+//                 else
+//                     dp[n - 1][j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
+//             }
+//         }
+        
+//         for(int i = n - 2; i >= 0; i--)
+//         {
+//             for(int j1 = 0; j1 < m; j1++)
+//             {
+//                 for(int j2 = 0; j2 < m; j2++)
+//                 {
+//                     vector<int> dj = {-1,0,1};
+//                     int maxi = -1e9;
+
+//                     for(auto r1: dj)
+//                     {            
+//                         for(auto r2: dj)
+//                         {                
+//                             int value = 0;
+
+//                             if(j1 == j2)
+//                                 value = grid[i][j1];
+//                             else
+//                                 value = grid[i][j2] + grid[i][j1];
+                            
+//                             if(j1 + r1 >= 0 && j1 + r1 < m && j2 + r2 >= 0 && j2 + r2 < m)
+//                                 value += dp[i + 1][j1 + r1][j2 + r2];
+//                             else
+//                                 value = -1e9;
+                            
+//                             maxi = max(maxi,value);
+//                         }
+//                     }
+//                     dp[i][j1][j2] = maxi;
+//                 }
+//             }
+//         }
+//         return dp[0][0][m - 1];
+//     }
 
 
 
