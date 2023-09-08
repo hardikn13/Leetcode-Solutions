0f213cc1d0d827c2ca81f4440cb397eb8440d191
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2023-09-08 15:40:33 +0530

    Time: 68 ms (75.20%), Space: 18.7 MB (42.92%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -3,31 +3,32 @@ public:
 
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+        vector<int> prev(amount + 1, 0), curr(amount + 1, 0);
         
         for(int t = 0; t <= amount; t++)
         {
             if(t % coins[0] == 0)
-                dp[0][t] = t / coins[0];
+                prev[t] = t / coins[0];
             else
-                dp[0][t] = 1e9;
+                prev[t] = 1e9;
         }
         
         for(int ind = 1; ind < n; ind++)
         {
             for(int t = 0; t <= amount; t++)
             {
-                int notTake = 0 + dp[ind - 1][t];
+                int notTake = 0 + prev[t];
                 int take = INT_MAX;
 
                 if(coins[ind] <= t)
-                    take = 1 + dp[ind][t - coins[ind]];
+                    take = 1 + curr[t - coins[ind]];
 
-                dp[ind][t] = min(take, notTake);
+                curr[t] = min(take, notTake);
             }
+            prev = curr;
         }
         
-        int ans = dp[n - 1][amount];
+        int ans = prev[amount];
         
         if(ans >= 1e9)
             return -1;
@@ -39,6 +40,46 @@ public:
 
 
 // TABULATION
+// ----------
+
+// int coinChange(vector<int>& coins, int amount) {
+//         int n = coins.size();
+//         vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+        
+//         for(int t = 0; t <= amount; t++)
+//         {
+//             if(t % coins[0] == 0)
+//                 dp[0][t] = t / coins[0];
+//             else
+//                 dp[0][t] = 1e9;
+//         }
+        
+//         for(int ind = 1; ind < n; ind++)
+//         {
+//             for(int t = 0; t <= amount; t++)
+//             {
+//                 int notTake = 0 + dp[ind - 1][t];
+//                 int take = INT_MAX;
+
+//                 if(coins[ind] <= t)
+//                     take = 1 + dp[ind][t - coins[ind]];
+
+//                 dp[ind][t] = min(take, notTake);
+//             }
+//         }
+        
+//         int ans = dp[n - 1][amount];
+        
+//         if(ans >= 1e9)
+//             return -1;
+//         return ans;
+//     }
+
+
+
+
+
+// MEMOIZATION
 // ----------
 
 // int f(int ind, int t, vector<int> &coins, vector<vector<int>> &dp)
