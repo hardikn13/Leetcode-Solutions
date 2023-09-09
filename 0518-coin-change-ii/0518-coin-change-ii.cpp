4ad73fa233b1188e5d49d00fc603354cbcb3a6c4
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2023-09-09 08:42:15 +0530

    Time: 21 ms (50.60%), Space: 7.4 MB (71.60%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -22,29 +22,83 @@ public:
     
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+        vector<int> prev(amount + 1, 0), curr(amount + 1, 0);
         
         for(int t = 0; t <= amount; t++)
         {
-            dp[0][t] = (t % coins[0] == 0);
+            prev[t] = (t % coins[0] == 0);
         }
         
         for(int ind = 1; ind < n; ind++)
         {
             for(int t = 0; t <= amount; t++)
             {
-                int notTake = dp[ind - 1][t];
+                int notTake = prev[t];
                 int take = 0;
 
                 if(coins[ind] <= t)
-                    take = dp[ind][t - coins[ind]];
+                    take = curr[t - coins[ind]];
 
-                dp[ind][t] = take + notTake;
+                curr[t] = take + notTake;
             }
+            prev = curr;
         }
-        return dp[n - 1][amount];
+        return prev[amount];
     }
 };
+
+
+
+
+
+// TABULATION
+// ----------
+
+// int f(int ind, int t, vector<int> &coins, vector<vector<int>> &dp)
+//     {
+//         if(ind == 0)
+//         {
+//             return t % coins[0] == 0;
+//         }
+        
+//         if(dp[ind][t] != -1)
+//             return dp[ind][t];
+        
+//         int notTake = f(ind - 1, t, coins, dp);
+//         int take = 0;
+        
+//         if(coins[ind] <= t)
+//             take = f(ind, t - coins[ind], coins, dp);
+        
+//         return dp[ind][t] = take + notTake;
+//     }
+    
+//     int change(int amount, vector<int>& coins) {
+//         int n = coins.size();
+//         vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+        
+//         for(int t = 0; t <= amount; t++)
+//         {
+//             dp[0][t] = (t % coins[0] == 0);
+//         }
+        
+//         for(int ind = 1; ind < n; ind++)
+//         {
+//             for(int t = 0; t <= amount; t++)
+//             {
+//                 int notTake = dp[ind - 1][t];
+//                 int take = 0;
+
+//                 if(coins[ind] <= t)
+//                     take = dp[ind][t - coins[ind]];
+
+//                 dp[ind][t] = take + notTake;
+//             }
+//         }
+//         return dp[n - 1][amount];
+//     }
+
+
 
 
 
