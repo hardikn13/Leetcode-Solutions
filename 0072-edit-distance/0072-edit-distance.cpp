1324963cd92b2cfb741fa8b968c034c564812f33
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2023-09-10 13:59:04 +0530

    Time: 16 ms (42.61%), Space: 9.2 MB (27.50%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -2,30 +2,68 @@ class Solution {
 public:
 
     
-    int f(int i, int j, string &text1, string &text2, vector<vector<int>> &dp)
-    {
-        if(i < 0)
-            return j + 1;
-        if(j < 0)
-            return i + 1;
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        if(text1[i] == text2[j])
-            return dp[i][j] = 0 + f(i - 1, j - 1, text1, text2, dp);
-        else
-            return dp[i][j] = 1 + min(f(i - 1, j, text1, text2, dp),
-                                      min(f(i, j - 1, text1, text2, dp),
-                                          f(i - 1, j - 1, text1, text2, dp)));
-    }
-    
-    
     int minDistance(string word1, string word2) {
+        
         int n = word1.size();
         int m = word2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
         
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return f(n - 1, m - 1, word1, word2, dp);
+        for(int i = 0; i <= n; i++)
+             dp[i][0] = i;
+        for(int j = 0; j <= m; j++)
+             dp[0][j] = j;
+        
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= m; j++)
+            {
+                if(word1[i - 1] == word2[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = 1 + min(dp[i - 1][j], min(dp[i - 1][j - 1], dp[i][j - 1]));
+                }
+            }
+        }
+        
+        return dp[n][m];
     }
 };
+
+
+
+
+
+
+
+// MEMOIZATION
+// -----------
+
+// int f(int i, int j, string &text1, string &text2, vector<vector<int>> &dp)
+//     {
+//         if(i < 0)
+//             return j + 1;
+//         if(j < 0)
+//             return i + 1;
+        
+//         if(dp[i][j] != -1)
+//             return dp[i][j];
+        
+//         if(text1[i] == text2[j])
+//             return dp[i][j] = 0 + f(i - 1, j - 1, text1, text2, dp);
+//         else
+//             return dp[i][j] = 1 + min(f(i - 1, j, text1, text2, dp), "DELETE"
+//                                       min(f(i, j - 1, text1, text2, dp), "INSERT"
+//                                           f(i - 1, j - 1, text1, text2, dp))); "REPLACE"
+//     }
+    
+    
+//     int minDistance(string word1, string word2) {
+//         int n = word1.size();
+//         int m = word2.size();
+        
+//         vector<vector<int>> dp(n, vector<int>(m, -1));
+//         return f(n - 1, m - 1, word1, word2, dp);
+//     }
