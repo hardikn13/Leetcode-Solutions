e4ac25f822e7729fa1c84d19fa56e3d34b65e8bf
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2023-09-14 11:04:08 +0530

    Time: 164 ms (77.55%), Space: 75.7 MB (79.90%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -4,24 +4,9 @@ public:
     int maxProfit(vector<int>& prices) {
         
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
-        
-        for(int ind = 0; ind < n; ind++)
-        {
-            for(int buy = 0; buy < 2; buy++)
-            {
-                dp[ind][buy][0] = 0;
-            }
-        }
-        
-        for(int buy = 0; buy < 2; buy++)
-        {
-            for(int cap = 0; cap < 3; cap++)
-            {
-                dp[n][buy][cap] = 0;
-            }
-        }
-        
+        vector<vector<int>> after(2, vector<int>(3, 0));
+        vector<vector<int>> curr(2, vector<int>(3, 0));
+
         for(int ind = n - 1; ind >= 0; ind--)
         {
             for(int buy = 0; buy < 2; buy++)
@@ -30,15 +15,55 @@ public:
                 {
                     
                     if(buy)
-                        dp[ind][buy][cap] = max(-prices[ind] + dp[ind + 1][0][cap], 0 + dp[ind + 1][1][cap]);
+                        curr[buy][cap] = max(-prices[ind] + after[0][cap], 0 + after[1][cap]);
                     else
-                        dp[ind][buy][cap] = max(prices[ind] + dp[ind + 1][1][cap - 1], dp[ind + 1][0][cap]);
+                        curr[buy][cap] = max(prices[ind] + after[1][cap - 1],after[0][cap]);
                 }
             }
+            after = curr;
         }
-        return dp[0][1][2];
+        return after[1][2];
     }
 };
+
+
+
+
+
+
+
+
+
+// TABULATION
+// ----------
+
+// int maxProfit(vector<int>& prices) {
+        
+//         int n = prices.size();
+//         vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+
+//         for(int ind = n - 1; ind >= 0; ind--)
+//         {
+//             for(int buy = 0; buy < 2; buy++)
+//             {
+//                 for(int cap = 1; cap < 3; cap++)
+//                 {
+                    
+//                     if(buy)
+//                         dp[ind][buy][cap] = max(-prices[ind] + dp[ind + 1][0][cap], 0 + dp[ind + 1][1][cap]);
+//                     else
+//                         dp[ind][buy][cap] = max(prices[ind] + dp[ind + 1][1][cap - 1], dp[ind + 1][0][cap]);
+//                 }
+//             }
+//         }
+//         return dp[0][1][2];
+//     }
+// };
+
+
+
+
+
 
 
 
