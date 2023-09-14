d229afa68aa905c40e7b95969716d62523a7c5f4
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2023-09-14 11:30:13 +0530

    Time: 24 ms (32.52%), Space: 14.2 MB (49.26%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,29 +1,68 @@
 class Solution {
 public:
+    
+    // NEW METHOD
+    // ==========
+    
+    int f(int ind, int transNo, int k, int n, vector<int>& prices, vector<vector<int>> &dp)
+    {
+        if(ind == n || transNo == 2 * k)
+            return 0;
+        
+        if(dp[ind][transNo] != -1)
+             return dp[ind][transNo];
+        
+        if(transNo % 2 == 0)
+            dp[ind][transNo] = max(-prices[ind] + f(ind + 1, transNo + 1, k, n, prices, dp), 0 + f(ind + 1, transNo, k, n, prices, dp));
+        else
+            dp[ind][transNo] = max(prices[ind] + f(ind + 1, transNo + 1, k, n, prices, dp), f(ind + 1, transNo, k, n, prices, dp));
+        
+        return dp[ind][transNo];
+    }
+    
     int maxProfit(int k, vector<int>& prices) {
         
         int n = prices.size();
-        vector<vector<int>> after(2, vector<int>(k + 1, 0));
-        vector<vector<int>> curr(2, vector<int>(k + 1, 0));
-
-        for(int ind = n - 1; ind >= 0; ind--)
-        {
-            for(int buy = 0; buy < 2; buy++)
-            {
-                for(int cap = 1; cap < k + 1; cap++)
-                {
-                    
-                    if(buy)
-                        curr[buy][cap] = max(-prices[ind] + after[0][cap], 0 + after[1][cap]);
-                    else
-                        curr[buy][cap] = max(prices[ind] + after[1][cap - 1],after[0][cap]);
-                }
-            }
-            after = curr;
-        }
-        return after[1][k];
+        vector<vector<int>> dp(n, vector<int>(2 * k, -1));
+        return f(0, 0, k, n, prices, dp);
     }
 };
+
+
+
+
+// STRIVER'S METHOD
+// ================
+
+
+// SPACE OPTIMIZED
+// ---------------
+
+// int maxProfit(int k, vector<int>& prices) {
+        
+//         int n = prices.size();
+//         vector<vector<int>> after(2, vector<int>(k + 1, 0));
+//         vector<vector<int>> curr(2, vector<int>(k + 1, 0));
+
+//         for(int ind = n - 1; ind >= 0; ind--)
+//         {
+//             for(int buy = 0; buy < 2; buy++)
+//             {
+//                 for(int cap = 1; cap < k + 1; cap++)
+//                 {
+                    
+//                     if(buy)
+//                         curr[buy][cap] = max(-prices[ind] + after[0][cap], 0 + after[1][cap]);
+//                     else
+//                         curr[buy][cap] = max(prices[ind] + after[1][cap - 1],after[0][cap]);
+//                 }
+//             }
+//             after = curr;
+//         }
+//         return after[1][k];
+//     }
+
+
 
 
 
