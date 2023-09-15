4592daa77a5b1eb7276a73ca636e8332625fa0df
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2023-09-15 18:39:16 +0530

    Time: 247 ms (57.00%), Space: 10.9 MB (36.97%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -4,24 +4,70 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         
         int n = nums.size();
-        vector<int> dp(n, 1);
-        int maxi = 1;
+        vector<int> dp(n, 1), hash(n);
+        int maxi = 1, lastIndex = 0;
         
         for(int ind = 0; ind < n; ind++)
         {
+            hash[ind] = ind;
             for(int prev = 0; prev < ind; prev++)
             {
-                if(nums[prev] < nums[ind])
-                    dp[ind] = max(dp[ind], 1 + dp[prev]);
+                if(nums[prev] < nums[ind] && 1 + dp[prev] > dp[ind])
+                {
+                    dp[ind] = 1 + dp[prev];
+                    hash[ind] = prev;
+                }
             }
-            maxi = max(maxi, dp[ind]);
+            if(dp[ind] > maxi)
+            {
+                maxi = dp[ind];
+                lastIndex = ind;
+            }
         }
+        
+        vector<int> temp;
+        temp.push_back(nums[lastIndex]);
+        
+        while(hash[lastIndex] != lastIndex)
+        {
+            lastIndex = hash[lastIndex];
+            temp.push_back(nums[lastIndex]);
+        }
+        
+        reverse(temp.begin(), temp.end());
         return maxi;
     }
 };
 
 
 
+
+// TABULATION
+// ----------
+
+// int lengthOfLIS(vector<int>& nums) {
+        
+//         int n = nums.size();
+//         vector<int> dp(n, 1);
+//         int maxi = 1;
+        
+//         for(int ind = 0; ind < n; ind++)
+//         {
+//             for(int prev = 0; prev < ind; prev++)
+//             {
+//                 if(nums[prev] < nums[ind])
+//                     dp[ind] = max(dp[ind], 1 + dp[prev]);
+//             }
+//             maxi = max(maxi, dp[ind]);
+//         }
+//         return maxi;
+//     }
+
+
+
+
+
+// =========================
 
 
 
