2b5433c55a611f3ef0b9073c69d60f5e9f8dbe94
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2023-09-16 16:37:29 +0530

    Time: 3 ms (93.20%), Space: 14.8 MB (5.59%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,54 +1,113 @@
 class Solution {
 public:
     
-    int lowerBound(vector<int> arr, int n, int x)
+    int firstOccurence(vector<int> nums, int n, int target)
     {
         int low = 0, high = n - 1;
-        int ans = n;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            // maybe an answer
-            if (arr[mid] >= x) {
-                ans = mid;
-                //look for smaller index on the left
+        int first = -1;
+        
+        while(low <= high)
+        {
+            int mid = (low + high)/2;
+            
+            if(nums[mid] == target)
+            {
+                first = mid;
                 high = mid - 1;
             }
-            else {
-                low = mid + 1; // look on the right
-            }
+            else if(nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
         }
-        return ans;
+        
+        return first;
     }
     
-    int upperBound(vector<int> &arr, int n, int x)
+    int lastOccurence(vector<int> nums, int n, int target)
     {
         int low = 0, high = n - 1;
-        int ans = n;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            // maybe an answer
-            if (arr[mid] > x) {
-                ans = mid;
-                //look for smaller index on the left
+        int last = -1;
+        
+        while(low <= high)
+        {
+            int mid = (low + high)/2;
+            
+            if(nums[mid] == target)
+            {
+                last = mid;
+                low = mid + 1;
+            }
+            else if(nums[mid] < target)
+                low = mid + 1;
+            else
                 high = mid - 1;
-            }
-            else {
-                low = mid + 1; // look on the right
-            }
         }
-        return ans;
+        
+        return last;
     }
 
     vector<int> searchRange(vector<int>& nums, int target) {
-        
         int n = nums.size();
-        int lb = lowerBound(nums, n, target);
-        
-        if(lb == n || nums[lb] != target)
-            return {-1, -1};
-        
-        return {lb, upperBound(nums, n, target) - 1};
+        return {firstOccurence(nums, n, target), lastOccurence(nums, n, target)};
     }
 };
+
+
+
+
+
+
+// USING LOWERBOUND & UPPERBOUND
+// -----------------------------
+
+// int lowerBound(vector<int> arr, int n, int x)
+//     {
+//         int low = 0, high = n - 1;
+//         int ans = n;
+
+//         while (low <= high) {
+//             int mid = (low + high) / 2;
+//             // maybe an answer
+//             if (arr[mid] >= x) {
+//                 ans = mid;
+//                 //look for smaller index on the left
+//                 high = mid - 1;
+//             }
+//             else {
+//                 low = mid + 1; // look on the right
+//             }
+//         }
+//         return ans;
+//     }
+    
+//     int upperBound(vector<int> &arr, int n, int x)
+//     {
+//         int low = 0, high = n - 1;
+//         int ans = n;
+
+//         while (low <= high) {
+//             int mid = (low + high) / 2;
+//             // maybe an answer
+//             if (arr[mid] > x) {
+//                 ans = mid;
+//                 //look for smaller index on the left
+//                 high = mid - 1;
+//             }
+//             else {
+//                 low = mid + 1; // look on the right
+//             }
+//         }
+//         return ans;
+//     }
+
+//     vector<int> searchRange(vector<int>& nums, int target) {
+        
+//         int n = nums.size();
+//         int lb = lowerBound(nums, n, target);
+        
+//         if(lb == n || nums[lb] != target)
+//             return {-1, -1};
+        
+//         return {lb, upperBound(nums, n, target) - 1};
+//     }
