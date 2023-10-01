bf1e5a4b81e1ca362ce8995efb98655b4d3b1bd8
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2023-10-01 19:08:02 +0530

    Time: 52 ms (16.08%), Space: 20.1 MB (6.39%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivgoogle-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivapple-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div85-maximal-rectangle/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivgoogle-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivapple-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div85-maximal-rectangle.cpp
@@ -0,0 +1,94 @@
+class Solution {
+    
+    private:
+    
+    vector<int> nextSmallerElement(vector<int> arr, int n)
+        {
+            stack<int> stk;
+            stk.push(-1);
+            vector<int> ans(n);
+            for(int i = n-1; i >= 0; i--)
+            {
+                while(stk.top() != -1 && arr[stk.top()] >= arr[i])
+                {
+                    stk.pop();
+                }
+                ans[i] = stk.top();
+                stk.push(i);
+            }
+            return ans;
+        }
+    
+    vector<int> prevSmallerElement(vector<int> arr, int n)
+        {
+            stack<int> stk;
+            stk.push(-1);
+            vector<int> ans(n);
+            for(int i = 0; i < n; i++)
+            {
+                while(stk.top() != -1 && arr[stk.top()] >= arr[i])
+                {
+                    stk.pop();
+                }
+                ans[i] = stk.top();
+                stk.push(i);
+            }
+            return ans;
+        }
+    
+    int largestRectangleArea(vector<int> heights, int n) {
+        int max = 0;
+        vector<int> next(n);
+        next = nextSmallerElement(heights, n);
+        vector<int> prev(n);
+        prev = prevSmallerElement(heights, n);
+        
+        for(int i = 0; i < n; i++)
+        {
+            int l = heights[i];
+            if(next[i] == -1)
+            {
+                next[i] = n;
+            }
+            int b = next[i] - prev[i] - 1;
+            int area = l * b;
+            if(area > max)
+            {
+                max = area;
+            }
+        }
+        return max;
+    }
+    
+    
+    public:
+    
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        cout<<matrix[0][0]<<endl;
+        
+        vector<int> v(m);
+        for(int i = 0; i < m; i++){
+            int a = int(matrix[0][i] - '0');
+            v[i] = a;
+        }
+        int area = largestRectangleArea(v,m);
+
+        
+        for(int i = 1; i < n; i++)
+        {
+            for(int j = 0; j < m; j++)
+            {
+                if(matrix[i][j] != '0'){
+                    v[j] += int(matrix[i][j] - '0'); 
+                }
+                else{
+                    v[j] = 0;
+                } 
+            }
+            area = max(area, largestRectangleArea(v, m));
+        }
+        return area;
+    }
+};
