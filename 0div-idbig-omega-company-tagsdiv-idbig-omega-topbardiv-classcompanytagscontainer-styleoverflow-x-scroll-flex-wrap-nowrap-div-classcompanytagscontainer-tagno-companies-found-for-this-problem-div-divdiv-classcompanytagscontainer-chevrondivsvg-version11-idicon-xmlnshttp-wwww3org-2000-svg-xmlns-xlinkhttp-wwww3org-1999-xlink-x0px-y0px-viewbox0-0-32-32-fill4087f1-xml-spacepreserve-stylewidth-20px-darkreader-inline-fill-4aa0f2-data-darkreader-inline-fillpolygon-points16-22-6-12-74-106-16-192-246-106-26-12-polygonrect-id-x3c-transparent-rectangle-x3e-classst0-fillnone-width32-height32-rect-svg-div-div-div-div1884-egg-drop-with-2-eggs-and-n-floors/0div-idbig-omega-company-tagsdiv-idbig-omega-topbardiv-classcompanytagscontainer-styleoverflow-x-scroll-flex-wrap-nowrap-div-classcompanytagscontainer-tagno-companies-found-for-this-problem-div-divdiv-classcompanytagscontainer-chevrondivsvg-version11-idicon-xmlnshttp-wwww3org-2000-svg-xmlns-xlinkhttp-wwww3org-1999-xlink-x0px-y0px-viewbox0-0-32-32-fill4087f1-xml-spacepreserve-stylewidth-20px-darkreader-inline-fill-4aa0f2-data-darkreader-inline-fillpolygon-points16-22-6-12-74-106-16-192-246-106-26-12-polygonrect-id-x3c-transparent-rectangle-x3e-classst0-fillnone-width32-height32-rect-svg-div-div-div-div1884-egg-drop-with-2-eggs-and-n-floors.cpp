b5c7fbf217a660ad065253be75fc26938c79c72b
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2023-12-23 10:17:53 +0530

    Time: 201 ms (6.64%), Space: 7.4 MB (22.73%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1884-egg-drop-with-2-eggs-and-n-floors/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1884-egg-drop-with-2-eggs-and-n-floors.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1884-egg-drop-with-2-eggs-and-n-floors/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -1,33 +1,81 @@
 class Solution {
 public:
-    int f(int eggs, int n, vector<vector<int>> &dp)
-    {
-        if(eggs == 1)
-            return n;
-        
-        if(n == 0 || n == 1)
-            return n;
-        
-        if(dp[eggs][n] != -1)
-            return dp[eggs][n];
-
-        int mini = INT_MAX;
-        for(int i = 1; i < n; i++)
-        {
-            int breaks = f(eggs - 1, i - 1, dp);
-            int notBreak = f(eggs, n - i, dp);
-            int ans = 1 + max(breaks, notBreak);
-            mini = min(mini, ans);
-        }
-        
-        return dp[eggs][n] = mini;
-        
-    }
-    
     int twoEggDrop(int n) {
-        
         int eggs = 2;
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-        return f(eggs, n, dp);
+        vector<vector<int>> dp(eggs + 1, vector<int>(n + 1, 0));
+
+        for (int i = 1; i <= eggs; i++) {
+            dp[i][0] = 0;
+            dp[i][1] = 1;
+        }
+
+        for (int i = 1; i <= n; i++)
+            dp[1][i] = i;
+
+        for (int i = 2; i <= eggs; i++) {
+            for (int j = 2; j <= n; j++) {
+                dp[i][j] = INT_MAX;
+                for (int k = 1; k <= j; k++) {
+                    int breaks = dp[i - 1][k - 1];
+                    int notBreak = dp[i][j - k];
+                    int maxDrops = 1 + max(breaks, notBreak);
+                    dp[i][j] = min(dp[i][j], maxDrops);
+                }
+            }
+        }
+
+        return dp[eggs][n];
     }
 };
+
+
+
+// FORMULA
+// -------
+
+// int twoEggDrop(int n) {
+//     int m = 1;
+//     while (m * (m + 1) / 2 < n)
+//         ++m;
+//     return m;    
+// }
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int f(int eggs, int n, vector<vector<int>> &dp)
+//     {
+//         if(eggs == 1)
+//             return n;
+        
+//         if(n == 0 || n == 1)
+//             return n;
+        
+//         if(dp[eggs][n] != -1)
+//             return dp[eggs][n];
+
+//         int mini = INT_MAX;
+//         for(int i = 1; i < n; i++)
+//         {
+//             int breaks = f(eggs - 1, i - 1, dp);
+//             int notBreak = f(eggs, n - i, dp);
+//             int ans = 1 + max(breaks, notBreak);
+//             mini = min(mini, ans);
+//         }
+        
+//         return dp[eggs][n] = mini;
+        
+//     }
+    
+//     int twoEggDrop(int n) {
+        
+//         int eggs = 2;
+//         vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        
+//         return f(eggs, n, dp);
+//     }
+// };
