e371ac5f3c24b8f42c05361698d6313429650591
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2024-01-01 10:40:44 +0530

    Time: 175 ms (5.53%), Space: 38.7 MB (38.34%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1262-greatest-sum-divisible-by-three/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1262-greatest-sum-divisible-by-three.cpp
@@ -0,0 +1,90 @@
+class Solution {
+public:
+    int maxSumDivThree(vector<int>& nums) {
+        
+        int n = nums.size(), remainder = 0;
+        vector<vector<int>> dp(n + 1, vector<int> (4, 0));
+        
+        dp[0][0] = 0;
+        dp[0][1] = INT_MIN;
+        dp[0][2] = INT_MIN;
+
+        for(int ind = 1; ind <= n; ind++)
+        {
+            for(int remainder = 0; remainder < 3; remainder++)
+            {
+                int notTake = dp[ind - 1][remainder];
+                int take = nums[ind - 1] + dp[ind - 1][(remainder + nums[ind - 1]) % 3];
+
+                dp[ind][remainder] = max(take, notTake);
+            }
+        }
+        
+        return dp[n][0];
+    }
+};
+
+
+
+
+
+
+// RECURSION
+// ---------
+
+// int f(int ind, vector<int> nums, int sum)
+//     {
+//         if(ind < 0)
+//         {
+//             if(sum % 3 == 0)
+//                 return sum;
+//             else
+//                 return INT_MIN;
+//         }
+        
+//         int notTake = f(ind - 1, nums, sum);
+//         int take = f(ind - 1, nums, sum + nums[ind]);
+        
+//         return max(take, notTake);
+//     }
+    
+//     int maxSumDivThree(vector<int>& nums) {
+        
+//         int n = nums.size(), sum = 0;
+//         return f(n - 1, nums, sum);
+//     }
+
+
+
+
+
+
+
+// MEMOIZATION
+// -----------
+
+// int f(int ind, vector<int> nums, int remainder, vector<vector<int>> &dp)
+//     {
+//         if(ind < 0)
+//         {
+//             if(remainder == 0)
+//                 return 0;
+//             else
+//                 return INT_MIN;
+//         }
+        
+//         if(dp[ind][remainder] != -1)
+//             return dp[ind][remainder];
+        
+//         int notTake = f(ind - 1, nums, remainder, dp);
+//         int take = nums[ind] + f(ind - 1, nums, (remainder + nums[ind]) % 3, dp);
+        
+//         return dp[ind][remainder] = max(take, notTake);
+//     }
+    
+//     int maxSumDivThree(vector<int>& nums) {
+        
+//         int n = nums.size(), remainder = 0;
+//         vector<vector<int>> dp(n + 1, vector<int> (4, -1));
+//         return f(n - 1, nums, remainder, dp);
+//     }
