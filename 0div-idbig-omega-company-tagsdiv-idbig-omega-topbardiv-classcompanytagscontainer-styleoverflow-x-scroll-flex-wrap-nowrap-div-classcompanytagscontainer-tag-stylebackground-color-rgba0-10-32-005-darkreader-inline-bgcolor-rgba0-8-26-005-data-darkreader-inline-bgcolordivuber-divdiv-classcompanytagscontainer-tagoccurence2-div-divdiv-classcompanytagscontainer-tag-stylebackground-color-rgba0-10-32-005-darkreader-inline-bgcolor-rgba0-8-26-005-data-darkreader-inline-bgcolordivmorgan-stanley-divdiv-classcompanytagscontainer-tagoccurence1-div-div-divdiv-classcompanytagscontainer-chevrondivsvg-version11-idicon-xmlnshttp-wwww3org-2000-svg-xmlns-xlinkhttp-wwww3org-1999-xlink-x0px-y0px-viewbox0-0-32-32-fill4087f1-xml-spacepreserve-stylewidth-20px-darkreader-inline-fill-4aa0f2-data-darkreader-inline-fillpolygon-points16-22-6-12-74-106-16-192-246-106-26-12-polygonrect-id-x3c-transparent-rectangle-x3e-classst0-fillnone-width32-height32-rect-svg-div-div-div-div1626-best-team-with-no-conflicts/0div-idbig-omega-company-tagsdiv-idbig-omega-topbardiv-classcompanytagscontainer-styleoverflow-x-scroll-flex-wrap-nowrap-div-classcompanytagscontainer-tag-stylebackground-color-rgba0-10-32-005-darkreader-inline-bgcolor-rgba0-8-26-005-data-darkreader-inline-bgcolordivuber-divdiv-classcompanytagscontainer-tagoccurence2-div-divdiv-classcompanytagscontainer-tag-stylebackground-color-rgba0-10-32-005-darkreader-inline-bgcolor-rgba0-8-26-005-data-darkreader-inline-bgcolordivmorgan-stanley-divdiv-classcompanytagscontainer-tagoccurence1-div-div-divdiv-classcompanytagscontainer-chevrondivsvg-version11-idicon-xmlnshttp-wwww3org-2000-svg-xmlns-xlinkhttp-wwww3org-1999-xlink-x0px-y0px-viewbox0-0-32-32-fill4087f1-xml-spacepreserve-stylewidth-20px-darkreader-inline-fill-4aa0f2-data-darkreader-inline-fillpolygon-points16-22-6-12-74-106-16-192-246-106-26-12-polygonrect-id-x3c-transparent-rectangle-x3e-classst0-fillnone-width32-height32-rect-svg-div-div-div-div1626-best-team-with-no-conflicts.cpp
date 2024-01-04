0e756b66dce676ad8078debc53b32b46d0df0820
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2024-01-04 08:56:53 +0530

    Time: 249 ms (37.44%), Space: 133.8 MB (25.39%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivuber-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivmorgan-stanley-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1626-best-team-with-no-conflicts/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivuber-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivmorgan-stanley-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1626-best-team-with-no-conflicts.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivuber-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivmorgan-stanley-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1626-best-team-with-no-conflicts/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivuber-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivmorgan-stanley-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1626-best-team-with-no-conflicts.cpp
@@ -1,21 +1,5 @@
 class Solution {
 public:
-    int f(int ind, int prevInd, vector<pair<int, int>>& v, vector<vector<int>> &dp)
-    {
-        if(ind >= v.size())
-            return 0;
-        
-        if(dp[ind][prevInd + 1] != -1)
-            return dp[ind][prevInd + 1];
-        
-        int notTake = f(ind + 1, prevInd, v, dp);
-        int take = 0;
-        if(prevInd == -1 || v[ind].second >= v[prevInd].second)
-            take = v[ind].second + f(ind + 1, ind, v, dp);
-            
-        return dp[ind][prevInd + 1] = max(take, notTake);
-    }
-    
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
         
         int n = scores.size();
@@ -27,8 +11,61 @@ public:
         }
         sort(v.begin(), v.end());
         
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
         
-        return f(0, -1, v, dp);
+        for(int ind = n - 1; ind >= 0; ind--)
+        {
+            for(int prevInd = ind - 1; prevInd >= -1; prevInd--)
+            {
+                int notTake = dp[ind + 1][prevInd + 1];
+                int take = 0;
+                if(prevInd == -1 || v[ind].second >= v[prevInd].second)
+                    take = v[ind].second + dp[ind + 1][ind + 1];
+
+                dp[ind][prevInd + 1] = max(take, notTake);
+            }
+        }
+        
+        return dp[0][-1 + 1];
     }
 };
+
+
+
+
+
+
+// MEMOIZATION
+// -----------
+
+// int f(int ind, int prevInd, vector<pair<int, int>>& v, vector<vector<int>> &dp)
+//     {
+//         if(ind >= v.size())
+//             return 0;
+        
+//         if(dp[ind][prevInd + 1] != -1)
+//             return dp[ind][prevInd + 1];
+        
+//         int notTake = f(ind + 1, prevInd, v, dp);
+//         int take = 0;
+//         if(prevInd == -1 || v[ind].second >= v[prevInd].second)
+//             take = v[ind].second + f(ind + 1, ind, v, dp);
+            
+//         return dp[ind][prevInd + 1] = max(take, notTake);
+//     }
+    
+//     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
+        
+//         int n = scores.size();
+//         vector<pair<int, int>> v;
+        
+//         for(int i = 0; i < n; i++)
+//         {
+//             v.push_back({ages[i], scores[i]});
+//         }
+//         sort(v.begin(), v.end());
+        
+//         vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        
+//         return f(0, -1, v, dp);
+//     }
