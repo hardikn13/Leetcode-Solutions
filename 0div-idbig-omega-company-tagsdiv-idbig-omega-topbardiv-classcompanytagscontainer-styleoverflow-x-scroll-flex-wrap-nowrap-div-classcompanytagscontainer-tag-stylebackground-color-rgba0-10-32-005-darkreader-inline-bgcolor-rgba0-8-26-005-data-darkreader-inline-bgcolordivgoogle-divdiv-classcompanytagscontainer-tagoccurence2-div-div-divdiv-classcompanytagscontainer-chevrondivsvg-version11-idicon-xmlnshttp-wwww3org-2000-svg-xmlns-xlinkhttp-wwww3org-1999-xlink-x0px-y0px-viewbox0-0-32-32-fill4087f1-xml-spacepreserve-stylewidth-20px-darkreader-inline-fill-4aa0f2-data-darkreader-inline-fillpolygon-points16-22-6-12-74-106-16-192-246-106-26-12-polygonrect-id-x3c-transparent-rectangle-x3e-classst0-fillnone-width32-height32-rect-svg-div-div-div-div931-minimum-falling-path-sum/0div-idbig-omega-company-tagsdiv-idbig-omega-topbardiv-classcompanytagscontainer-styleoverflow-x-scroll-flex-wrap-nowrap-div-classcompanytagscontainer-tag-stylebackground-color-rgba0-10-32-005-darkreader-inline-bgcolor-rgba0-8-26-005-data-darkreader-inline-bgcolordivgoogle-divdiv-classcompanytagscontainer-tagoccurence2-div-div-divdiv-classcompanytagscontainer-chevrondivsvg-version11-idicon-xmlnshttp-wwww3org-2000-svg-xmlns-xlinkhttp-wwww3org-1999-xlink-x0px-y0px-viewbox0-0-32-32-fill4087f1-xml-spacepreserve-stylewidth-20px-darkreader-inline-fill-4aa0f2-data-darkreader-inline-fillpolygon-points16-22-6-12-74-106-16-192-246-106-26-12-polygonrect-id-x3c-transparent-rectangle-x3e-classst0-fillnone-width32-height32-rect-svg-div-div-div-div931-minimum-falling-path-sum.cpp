1a334fd331b673db98194bc771f1cd68e709aa9d
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2024-01-20 05:27:19 +0530

    Time: 11 ms (80.62%), Space: 13.1 MB (5.08%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div931-minimum-falling-path-sum/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div931-minimum-falling-path-sum.cpp
@@ -0,0 +1,99 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>> dp(n, vector<int> (m + 1, 0));
+        
+        int mini = INT_MAX;
+        for(int j = 0; j < m; j++)
+        {
+            dp[n - 1][j] = matrix[n - 1][j];
+            mini = min(mini, dp[n - 1][j]);
+        }
+        
+        dp[n - 1][m] = mini;
+        
+        for(int row = n - 2; row >= 0; row--)
+        {
+            mini = INT_MAX;
+            for(int col = 0; col <= m; col++)
+            {
+                if(col == m)
+                {
+                    dp[row][col] = mini;
+                    continue;
+                }
+                
+                int t1 = INT_MAX, t2 = INT_MAX, t3 = INT_MAX;
+        
+                t2 = matrix[row][col] + dp[row + 1][col];
+                if(col > 0)
+                    t1 = matrix[row][col] + dp[row + 1][col - 1];
+
+                if(col < m - 1)
+                    t3 = matrix[row][col] + dp[row + 1][col + 1];
+
+                dp[row][col] = min(t1, min(t2, t3));
+                mini = min(mini, dp[row][col]);
+            }
+        }
+        
+        
+        
+        return dp[0][m];
+    }
+};
+
+
+
+
+
+
+
+
+
+// MEMOIZATION
+// -----------
+
+// int f(int row, int col, vector<vector<int>>& matrix, vector<vector<int>> &dp)
+//     {
+//         int n = matrix.size();
+//         int m = matrix[0].size();
+        
+//         if(row == n)
+//             return 0;
+        
+//         if(col < 0 || col > m - 1)
+//             return INT_MAX;
+        
+//         if(dp[row][col] != -1)
+//             return dp[row][col];
+        
+//         int t1 = INT_MAX, t2 = INT_MAX, t3 = INT_MAX;
+        
+//         t2 = matrix[row][col] + f(row + 1, col, matrix, dp);
+//         if(col > 0)
+//             t1 = matrix[row][col] + f(row + 1, col - 1, matrix, dp);
+            
+//         if(col < m - 1)
+//             t3 = matrix[row][col] + f(row + 1, col + 1, matrix, dp);
+        
+//         return dp[row][col] = min(t1, min(t2, t3));
+//     }
+    
+//     int minFallingPathSum(vector<vector<int>>& matrix) {
+        
+//         int n = matrix.size();
+//         int m = matrix[0].size();
+//         vector<vector<int>> dp(n + 1, vector<int> (m + 1, -1));
+        
+//         int ans = INT_MAX;
+//         for(int j = 0; j < m; j++)
+//         {
+//             ans = min(ans, f(0, j, matrix, dp));
+//         }
+        
+//         return ans;
+//     }
