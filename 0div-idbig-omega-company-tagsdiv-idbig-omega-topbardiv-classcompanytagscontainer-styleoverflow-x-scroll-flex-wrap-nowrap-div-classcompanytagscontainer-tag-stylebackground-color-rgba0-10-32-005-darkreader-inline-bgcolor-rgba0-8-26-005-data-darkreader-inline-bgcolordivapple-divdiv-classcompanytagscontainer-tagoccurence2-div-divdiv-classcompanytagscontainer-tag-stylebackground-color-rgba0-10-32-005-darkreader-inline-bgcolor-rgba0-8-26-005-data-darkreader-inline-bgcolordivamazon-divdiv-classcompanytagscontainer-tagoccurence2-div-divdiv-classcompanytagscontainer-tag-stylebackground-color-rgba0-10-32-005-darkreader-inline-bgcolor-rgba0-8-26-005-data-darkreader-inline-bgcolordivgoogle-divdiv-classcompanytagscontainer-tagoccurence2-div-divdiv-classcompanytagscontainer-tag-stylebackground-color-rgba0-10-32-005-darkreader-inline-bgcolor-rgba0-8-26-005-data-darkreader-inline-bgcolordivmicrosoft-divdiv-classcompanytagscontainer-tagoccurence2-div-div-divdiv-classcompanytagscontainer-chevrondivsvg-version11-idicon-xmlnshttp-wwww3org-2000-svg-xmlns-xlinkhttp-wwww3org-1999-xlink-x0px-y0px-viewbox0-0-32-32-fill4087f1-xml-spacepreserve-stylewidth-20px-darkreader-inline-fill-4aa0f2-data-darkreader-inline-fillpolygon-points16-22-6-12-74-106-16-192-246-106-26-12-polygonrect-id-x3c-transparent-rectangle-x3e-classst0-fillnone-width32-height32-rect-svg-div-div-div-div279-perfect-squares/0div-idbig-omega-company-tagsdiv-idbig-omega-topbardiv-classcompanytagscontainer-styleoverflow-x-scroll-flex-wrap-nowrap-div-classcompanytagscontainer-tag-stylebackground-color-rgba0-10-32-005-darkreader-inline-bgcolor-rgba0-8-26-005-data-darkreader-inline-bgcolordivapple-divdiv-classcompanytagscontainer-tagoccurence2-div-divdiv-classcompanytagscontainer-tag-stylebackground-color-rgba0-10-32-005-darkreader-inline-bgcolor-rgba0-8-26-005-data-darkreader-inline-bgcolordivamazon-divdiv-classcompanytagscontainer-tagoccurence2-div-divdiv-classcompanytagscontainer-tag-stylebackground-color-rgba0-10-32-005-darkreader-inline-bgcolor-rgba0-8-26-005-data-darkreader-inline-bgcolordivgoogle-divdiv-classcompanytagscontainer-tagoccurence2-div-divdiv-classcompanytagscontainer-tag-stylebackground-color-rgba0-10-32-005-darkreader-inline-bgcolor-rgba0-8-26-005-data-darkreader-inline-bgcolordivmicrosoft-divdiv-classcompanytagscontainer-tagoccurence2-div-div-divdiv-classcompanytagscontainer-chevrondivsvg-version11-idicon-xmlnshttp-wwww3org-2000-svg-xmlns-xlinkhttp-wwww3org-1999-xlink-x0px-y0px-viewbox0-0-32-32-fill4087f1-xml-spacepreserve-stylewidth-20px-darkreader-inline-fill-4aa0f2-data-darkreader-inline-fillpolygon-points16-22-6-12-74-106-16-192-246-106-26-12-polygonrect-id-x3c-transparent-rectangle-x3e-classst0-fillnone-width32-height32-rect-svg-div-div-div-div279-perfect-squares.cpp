a474b2395cfd0883f3a5fd10b8f978a93a8e0968
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2024-02-08 15:17:55 +0530

    Time: 70 ms (76.65%), Space: 10.9 MB (28.71%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivapple-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivmicrosoft-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div279-perfect-squares/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivapple-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivmicrosoft-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div279-perfect-squares.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivapple-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivmicrosoft-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div279-perfect-squares/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivapple-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivmicrosoft-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div279-perfect-squares.cpp
@@ -4,25 +4,113 @@
 class Solution {
 public:
     int numSquares(int n) {
-        // Precompute perfect squares up to n
-        std::vector<int> squares;
-        for (int i = 1; i * i <= n; i++) {
+        
+        vector<int> squares;
+        for(int i = 1; i * i <= n; i++)
             squares.push_back(i * i);
-        }
 
-        // Initialize dp array
-        std::vector<int> dp(n + 1, INT_MAX);
+        vector<int> dp(n + 1, INT_MAX);
         dp[0] = 0;
 
-        // Dynamic programming to find the minimum number of perfect squares
-        for (int i = 1; i <= n; i++) {
-            for (int square : squares) {
-                if (i - square >= 0) {
-                    dp[i] = std::min(dp[i], 1 + dp[i - square]);
-                }
+        for (int i = 1; i <= n; i++)
+        {
+            for(int square : squares)
+            {
+                if(i - square >= 0)
+                    dp[i] = min(dp[i], 1 + dp[i - square]);
             }
         }
 
         return dp[n];
     }
 };
+
+
+
+
+
+// MEMOIZATION
+// -----------
+
+// bool isPerfectSquare(int n) {
+//         int root = sqrt(n);
+//         return root * root == n;
+//     }
+
+//     int f(int ind, vector<int> &dp) {
+//         if (ind <= 0)
+//             return 0;
+        
+//         if(dp[ind] != -1)
+//             return dp[ind];
+
+//         int count = INT_MAX;
+
+//         for(int i = ind; i > 0; i--)
+//         {
+//             if(isPerfectSquare(i))
+//                 count = min(count, 1 + f(ind - i, dp));
+//         }
+
+//         return dp[ind] = count;
+//     }
+
+//     int numSquares(int n) {
+        
+//         vector<int> dp(n + 1, -1);
+//         return f(n, dp);
+        
+//     }
+
+
+
+
+
+// MATHEMATICAL
+// ------------
+
+// class Solution 
+// {  
+// private:  
+//     int is_square(int n)
+//     {  
+//         int sqrt_n = (int)(sqrt(n));  
+//         return (sqrt_n*sqrt_n == n);  
+//     }
+    
+// public:
+//     // Based on Lagrange's Four Square theorem, there 
+//     // are only 4 possible results: 1, 2, 3, 4.
+//     int numSquares(int n) 
+//     {  
+//         // If n is a perfect square, return 1.
+//         if(is_square(n)) 
+//         {
+//             return 1;  
+//         }
+        
+//         // The result is 4 if and only if n can be written in the 
+//         // form of 4^k*(8*m + 7). Please refer to 
+//         // Legendre's three-square theorem.
+//         while ((n & 3) == 0) // n%4 == 0  
+//         {
+//             n >>= 2;  
+//         }
+//         if ((n & 7) == 7) // n%8 == 7
+//         {
+//             return 4;
+//         }
+        
+//         // Check whether 2 is the result.
+//         int sqrt_n = (int)(sqrt(n)); 
+//         for(int i = 1; i <= sqrt_n; i++)
+//         {  
+//             if (is_square(n - i*i)) 
+//             {
+//                 return 2;  
+//             }
+//         }  
+        
+//         return 3;  
+//     }  
+// };
