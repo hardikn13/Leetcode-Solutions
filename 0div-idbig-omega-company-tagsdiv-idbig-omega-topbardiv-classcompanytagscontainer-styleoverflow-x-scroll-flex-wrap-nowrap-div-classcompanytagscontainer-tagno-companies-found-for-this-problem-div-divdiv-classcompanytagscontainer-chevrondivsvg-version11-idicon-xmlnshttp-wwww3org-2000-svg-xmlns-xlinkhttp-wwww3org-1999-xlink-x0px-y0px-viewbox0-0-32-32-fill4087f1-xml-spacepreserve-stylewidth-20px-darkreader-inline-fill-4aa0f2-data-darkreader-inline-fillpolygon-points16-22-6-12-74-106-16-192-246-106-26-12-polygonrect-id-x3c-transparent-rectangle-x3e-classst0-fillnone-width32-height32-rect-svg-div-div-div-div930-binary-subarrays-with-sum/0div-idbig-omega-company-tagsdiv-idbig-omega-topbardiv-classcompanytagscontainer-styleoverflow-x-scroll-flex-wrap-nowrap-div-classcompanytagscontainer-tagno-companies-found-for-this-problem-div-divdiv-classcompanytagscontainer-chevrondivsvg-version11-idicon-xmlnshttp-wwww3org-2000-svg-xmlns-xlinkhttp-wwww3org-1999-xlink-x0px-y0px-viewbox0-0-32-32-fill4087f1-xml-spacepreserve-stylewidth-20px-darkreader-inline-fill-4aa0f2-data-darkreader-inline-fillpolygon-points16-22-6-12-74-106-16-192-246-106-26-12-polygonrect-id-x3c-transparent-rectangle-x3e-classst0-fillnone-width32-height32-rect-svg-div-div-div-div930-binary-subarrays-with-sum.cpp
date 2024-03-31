c24da32fb613179678a4990dcd4ceb1205c9c3fc
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2024-03-31 22:45:36 +0530

    Time: 31 ms (67.47%), Space: 38.2 MB (53.78%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div930-binary-subarrays-with-sum/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div930-binary-subarrays-with-sum.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div930-binary-subarrays-with-sum/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div930-binary-subarrays-with-sum.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int start = 0;
-        int prefixZeros = 0;
-        int currentSum = 0;
-        int totalCount = 0;
         
-        // Loop through the array using end pointer
-        for (int end = 0; end < nums.size(); ++end) {
-            // Add current element to the sum
-            currentSum += nums[end];
+        int n = nums.size();
+        unordered_map<int, int> freq;
+        freq[0] = 1;
+        int pref = 0, ans = 0;
+        
+        for(int i = 0; i < n; i++)
+        {
+            pref += nums[i];
             
-            // Slide the window while condition is met
-            while (start < end && (nums[start] == 0 || currentSum > goal)) {
-                if (nums[start] == 1) {
-                    prefixZeros = 0;
-                } else {
-                    prefixZeros += 1;
-                }
-                
-                currentSum -= nums[start];
-                start += 1;
-            }
+            if(freq.find(pref - goal) != freq.end())
+                ans += freq[pref - goal];
             
-            // Count subarrays when window sum matches the goal
-            if (currentSum == goal) {
-                totalCount += 1 + prefixZeros;
-            }
+            freq[pref]++;
         }
         
-        return totalCount;
+        return ans;
     }
 };
