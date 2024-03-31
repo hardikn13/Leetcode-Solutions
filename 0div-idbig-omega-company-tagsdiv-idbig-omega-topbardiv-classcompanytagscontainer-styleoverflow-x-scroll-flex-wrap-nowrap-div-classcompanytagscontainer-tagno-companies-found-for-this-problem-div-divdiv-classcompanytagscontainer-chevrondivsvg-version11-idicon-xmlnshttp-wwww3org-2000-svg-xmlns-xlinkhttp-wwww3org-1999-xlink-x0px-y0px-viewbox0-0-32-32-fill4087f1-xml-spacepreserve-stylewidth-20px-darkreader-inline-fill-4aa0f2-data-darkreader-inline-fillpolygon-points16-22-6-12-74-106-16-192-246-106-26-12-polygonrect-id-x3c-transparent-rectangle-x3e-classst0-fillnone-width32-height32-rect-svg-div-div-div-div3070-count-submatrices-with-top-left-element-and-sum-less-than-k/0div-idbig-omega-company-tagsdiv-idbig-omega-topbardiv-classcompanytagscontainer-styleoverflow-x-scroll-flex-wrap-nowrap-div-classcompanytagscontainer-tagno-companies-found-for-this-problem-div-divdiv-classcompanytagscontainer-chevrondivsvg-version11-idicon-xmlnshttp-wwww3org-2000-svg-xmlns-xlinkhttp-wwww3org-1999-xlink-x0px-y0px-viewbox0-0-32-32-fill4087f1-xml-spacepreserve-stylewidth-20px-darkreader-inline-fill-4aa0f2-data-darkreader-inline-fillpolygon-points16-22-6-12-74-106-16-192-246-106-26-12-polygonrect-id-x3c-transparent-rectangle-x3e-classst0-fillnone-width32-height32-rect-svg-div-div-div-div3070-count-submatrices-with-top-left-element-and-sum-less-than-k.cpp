32f87946f6c8b917f3d980d385dd30f63259539b
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2024-03-31 23:21:54 +0530

    Time: 156 ms (98.08%), Space: 89 MB (65.16%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3070-count-submatrices-with-top-left-element-and-sum-less-than-k/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int countSubmatrices(vector<vector<int>>& grid, int k) {
+        
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        int result = 0;
+        
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 1; j < m; j++)
+            {
+                grid[i][j] += grid[i][j - 1];
+            }
+        }
+        
+        for(int j = 0; j < m; j++)
+        {
+            int add = 0;
+            for(int i = 0; i < n; i++)
+            {
+                add += grid[i][j];
+                if(add <= k)
+                    result++;
+            }
+        }
+        
+        return result;
+    }
+};
