2c4e38355f7083fccfa48ef5b092c52a03263378
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2024-04-01 12:27:45 +0530

    Time: 10 ms (83.73%), Space: 24.3 MB (70.71%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3082-find-the-sum-of-the-power-of-all-subsequences/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3082-find-the-sum-of-the-power-of-all-subsequences.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3082-find-the-sum-of-the-power-of-all-subsequences/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div3082-find-the-sum-of-the-power-of-all-subsequences.cpp
@@ -22,31 +22,89 @@ public:
     }
     
     int mod = 1e9 + 7;
-    int f(long long ind, long long cnt, long long sum, vector<int> &nums, int k, vector<vector<vector<long long>>> &dp)
+    int f(long long ind, long long sum, vector<int> &nums, int k, vector<vector<long long>> &dp)
     {
         int n = nums.size();
         
         if(sum == k)
-            return power(2ll, (long long)n - cnt);
+            return power(2ll, (long long)n - ind);
         else if(sum > k)
             return 0;
         if(ind >= n)
             return 0;
         
-        if(dp[ind][cnt][sum] != -1)
-            return dp[ind][cnt][sum];
+        if(dp[ind][sum] != -1)
+            return dp[ind][sum];
         
-        long long take = f(ind + 1, cnt + 1, sum + nums[ind], nums, k, dp);
-        long long notTake = f(ind + 1, cnt, sum, nums, k, dp);
+        long long take = f(ind + 1, sum + nums[ind], nums, k, dp);
+        long long notTake = 2 * f(ind + 1, sum, nums, k, dp);
         
-        return dp[ind][cnt][sum] = (take + notTake) % mod;
+        return dp[ind][sum] = (take + notTake) % mod;
     }
     
     int sumOfPower(vector<int>& nums, int k) {
         
         int n = nums.size();
-        vector<vector<vector<long long>>> dp(n + 1, vector<vector<long long>>(k + 1, vector<long long>(k + 1, -1)));
+        vector<vector<long long>> dp(n + 1, vector<long long>(k + 1, -1));
         
-        return f(0, 0, 0, nums, k, dp);
+        return f(0, 0, nums, k, dp);
     }
 };
+
+
+
+// USING 3D DP
+// -----------
+
+// class Solution {
+// public:
+    
+//     long long power(long long a, long long n)
+//     {
+//         long long res = 1;
+//         while(n > 0)
+//         {
+//             if(n % 2)
+//             {
+//                 res = (res * a) % mod;
+//                 n--;
+//             }
+//             else
+//             {
+//                 a = (a * a) % mod;
+//                 n = n / 2;
+//             }
+//         }
+        
+//         return res % mod;
+//     }
+    
+//     int mod = 1e9 + 7;
+//     int f(long long ind, long long cnt, long long sum, vector<int> &nums, int k, vector<vector<vector<long long>>> &dp)
+//     {
+//         int n = nums.size();
+        
+//         if(sum == k)
+//             return power(2ll, (long long)n - cnt);
+//         else if(sum > k)
+//             return 0;
+//         if(ind >= n)
+//             return 0;
+        
+//         if(dp[ind][cnt][sum] != -1)
+//             return dp[ind][cnt][sum];
+        
+//         long long take = f(ind + 1, cnt + 1, sum + nums[ind], nums, k, dp);
+//         long long notTake = f(ind + 1, cnt, sum, nums, k, dp);
+        
+//         return dp[ind][cnt][sum] = (take + notTake) % mod;
+//     }
+    
+//     int sumOfPower(vector<int>& nums, int k) {
+        
+//         int n = nums.size();
+//         vector<vector<vector<long long>>> dp(n + 1, vector<vector<long long>>(k + 1, vector<long long>(k + 1, -1)));
+        
+//         return f(0, 0, 0, nums, k, dp);
+//     }
+// };
