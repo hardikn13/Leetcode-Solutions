3a146baa3968475fbeaade721bed88c0d369aca6
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2024-05-02 03:03:04 +0530

    Time: 0 ms (100.00%), Space: 11.9 MB (57.23%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div137-single-number-ii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div137-single-number-ii.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div137-single-number-ii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivamazon-divdiv-classcompanytagscontainer-tagoccurence5-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div137-single-number-ii.cpp
@@ -2,24 +2,49 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         
-        int ans = 0;
-        for(int k = 0; k < 32; k++)
+        int ones = 0, twos = 0, threes = 0;
+        for(int i = 0; i < nums.size(); i++)
         {
-            int temp = (1 << k);
-            
-            int cntZeroes = 0, cntOnes = 0;
-            for(auto it : nums)
-            {
-                if((it & temp) == 0)
-                    cntZeroes++;
-                else
-                    cntOnes++;
-            }
-            
-            if(cntOnes % 3 == 1)
-                ans = (ans | temp);
+            ones = (ones ^ nums[i]) & ~(twos);
+            twos = (twos ^ nums[i]) & ~(ones);
+            threes = (threes ^ nums[i]) & ~(twos);
         }
         
-        return ans;
+        return ones;
     }
 };
+
+
+
+
+
+
+
+// ANOTHER APPROACH
+// ----------------
+
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+        
+//         int ans = 0;
+//         for(int k = 0; k < 32; k++)
+//         {
+//             int temp = (1 << k);
+            
+//             int cntZeroes = 0, cntOnes = 0;
+//             for(auto it : nums)
+//             {
+//                 if((it & temp) == 0)
+//                     cntZeroes++;
+//                 else
+//                     cntOnes++;
+//             }
+            
+//             if(cntOnes % 3 == 1)
+//                 ans = (ans | temp);
+//         }
+        
+//         return ans;
+//     }
+// };
