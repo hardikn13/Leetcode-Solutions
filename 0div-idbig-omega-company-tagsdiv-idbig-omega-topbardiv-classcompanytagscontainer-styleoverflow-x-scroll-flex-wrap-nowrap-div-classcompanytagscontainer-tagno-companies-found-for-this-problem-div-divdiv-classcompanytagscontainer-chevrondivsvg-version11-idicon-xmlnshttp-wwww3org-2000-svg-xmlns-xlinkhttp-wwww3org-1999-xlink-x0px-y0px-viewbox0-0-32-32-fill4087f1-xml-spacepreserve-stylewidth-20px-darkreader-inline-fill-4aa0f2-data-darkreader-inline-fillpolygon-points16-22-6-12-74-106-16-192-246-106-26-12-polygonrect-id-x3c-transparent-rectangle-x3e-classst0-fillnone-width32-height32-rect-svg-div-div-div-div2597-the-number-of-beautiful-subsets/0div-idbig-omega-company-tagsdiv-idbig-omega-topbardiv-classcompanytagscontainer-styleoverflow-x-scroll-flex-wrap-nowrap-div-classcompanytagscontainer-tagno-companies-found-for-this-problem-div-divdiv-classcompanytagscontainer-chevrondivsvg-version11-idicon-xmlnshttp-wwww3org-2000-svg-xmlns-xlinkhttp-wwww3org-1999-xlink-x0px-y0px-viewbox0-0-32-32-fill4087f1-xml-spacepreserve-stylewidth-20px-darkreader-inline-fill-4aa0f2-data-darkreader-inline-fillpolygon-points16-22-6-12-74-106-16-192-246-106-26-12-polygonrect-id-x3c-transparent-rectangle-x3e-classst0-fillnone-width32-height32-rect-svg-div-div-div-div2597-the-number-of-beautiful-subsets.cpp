4ce4604ccb8bdf245c48f9f42b548b3d52983f22
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2024-05-23 12:40:14 +0530

    Time: 7 ms (97.73%), Space: 38.5 MB (41.32%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2597-the-number-of-beautiful-subsets/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2597-the-number-of-beautiful-subsets.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2597-the-number-of-beautiful-subsets/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2597-the-number-of-beautiful-subsets.cpp
@@ -1,28 +1,76 @@
 class Solution {
 public:
-    void f(int ind, int &ans, vector<int> &nums, int k, unordered_map<int, int> &mp)
+    int f(int ind, vector<pair<int, int>> &v, int k, vector<int> &dp)
     {
-        if(ind == nums.size())
-            ans++;
+        if(ind == v.size())
+            return 1;
+        
+        if(dp[ind] != -1)
+            return dp[ind];
+        
+        int notTake = f(ind + 1, v, k, dp);
+        int take = (1 << v[ind].second) - 1;
+        
+        if(ind + 1 < v.size() && v[ind + 1].first  - v[ind].first == k)
+            take *= f(ind + 2, v, k, dp);
         else
-        {
-            if(!mp[nums[ind] - k] && !mp[nums[ind] + k])
-            {
-                mp[nums[ind]]++;
-                f(ind + 1, ans, nums, k, mp);
-                mp[nums[ind]]--;
-            }
-            
-            f(ind + 1, ans, nums, k, mp);
-        }
+            take *= f(ind + 1, v, k, dp);
+        
+        return take + notTake;
     }
     
     int beautifulSubsets(vector<int>& nums, int k) {
         int n = nums.size();
-        int ans = 0;
-        unordered_map<int, int> mp;
-        f(0, ans, nums, k, mp);
+        int ans = 1;
+        
+        map<int, map<int, int>> freq;
+        for(int &it : nums)
+        {
+            freq[it % k][it]++;
+        }
+        
+        for(auto &it : freq)
+        {
+            vector<pair<int, int>> v(it.second.begin(), it.second.end());
+            vector<int> dp(v.size(), -1);
+            ans *= f(0, v, k, dp);
+        }
         
         return ans - 1;
     }
 };
+
+
+
+
+
+
+
+// ANOTHER APPROACH
+// ----------------
+
+// void f(int ind, int &ans, vector<int> &nums, int k, unordered_map<int, int> &mp)
+//     {
+//         if(ind == nums.size())
+//             ans++;
+//         else
+//         {
+//             if(!mp[nums[ind] - k] && !mp[nums[ind] + k])
+//             {
+//                 mp[nums[ind]]++;
+//                 f(ind + 1, ans, nums, k, mp);
+//                 mp[nums[ind]]--;
+//             }
+            
+//             f(ind + 1, ans, nums, k, mp);
+//         }
+//     }
+    
+//     int beautifulSubsets(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         int ans = 0;
+//         unordered_map<int, int> mp;
+//         f(0, ans, nums, k, mp);
+        
+//         return ans - 1;
+//     }
