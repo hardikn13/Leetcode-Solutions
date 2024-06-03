9065c9bbbc045da7e7d23a510567a11d60fae133
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2024-06-03 16:03:29 +0530

    Time: 5 ms (36.36%), Space: 8.9 MB (99.14%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivmicrosoft-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div352-data-stream-as-disjoint-intervals/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivmicrosoft-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div352-data-stream-as-disjoint-intervals.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivmicrosoft-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div352-data-stream-as-disjoint-intervals/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivmicrosoft-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div352-data-stream-as-disjoint-intervals.cpp
@@ -1,35 +1,85 @@
 class SummaryRanges {
 public:
-    set<int> st;
+    map<int, int> mp;
     SummaryRanges() {
         
     }
     
     void addNum(int value) {
-        st.insert(value);
+        int left = value;
+        int right = value;
+        
+        auto it = mp.upper_bound(value);
+        
+        if(it != mp.begin())
+        {
+            auto before = it;
+            before--;
+            
+            if(before -> second >= value)
+                return;
+            
+            if(before -> second == value - 1)
+                left = before -> first;
+        }
+        
+        if(it != mp.end() && it -> first == right + 1)
+        {
+            right = it -> second;
+            mp.erase(it);
+        }
+        
+        mp[left] = right;
     }
+    
     
     vector<vector<int>> getIntervals() {
         vector<vector<int>> ans;
-        if(st.empty())
-            return ans;
         
-        int mini = INT_MAX;
-        for(auto it : st)
-        {
-            mini = min(mini, it);
-            if(st.count(it + 1))
-                continue;
-            else
-            {
-                ans.push_back({mini, it});
-                mini = INT_MAX;
-            }
-        }
-        
+        for(auto it : mp)
+            ans.push_back({it.first, it.second});
+            
         return ans;
     }
 };
+
+
+
+
+// ANOTHER APPROACH
+// ----------------
+// class SummaryRanges {
+// public:
+//     set<int> st;
+//     SummaryRanges() {
+        
+//     }
+    
+//     void addNum(int value) {
+//         st.insert(value);
+//     }
+    
+//     vector<vector<int>> getIntervals() {
+//         vector<vector<int>> ans;
+//         if(st.empty())
+//             return ans;
+        
+//         int mini = INT_MAX;
+//         for(auto it : st)
+//         {
+//             mini = min(mini, it);
+//             if(st.count(it + 1))
+//                 continue;
+//             else
+//             {
+//                 ans.push_back({mini, it});
+//                 mini = INT_MAX;
+//             }
+//         }
+        
+//         return ans;
+//     }
+// };
 
 /**
  * Your SummaryRanges object will be instantiated and called as such:
