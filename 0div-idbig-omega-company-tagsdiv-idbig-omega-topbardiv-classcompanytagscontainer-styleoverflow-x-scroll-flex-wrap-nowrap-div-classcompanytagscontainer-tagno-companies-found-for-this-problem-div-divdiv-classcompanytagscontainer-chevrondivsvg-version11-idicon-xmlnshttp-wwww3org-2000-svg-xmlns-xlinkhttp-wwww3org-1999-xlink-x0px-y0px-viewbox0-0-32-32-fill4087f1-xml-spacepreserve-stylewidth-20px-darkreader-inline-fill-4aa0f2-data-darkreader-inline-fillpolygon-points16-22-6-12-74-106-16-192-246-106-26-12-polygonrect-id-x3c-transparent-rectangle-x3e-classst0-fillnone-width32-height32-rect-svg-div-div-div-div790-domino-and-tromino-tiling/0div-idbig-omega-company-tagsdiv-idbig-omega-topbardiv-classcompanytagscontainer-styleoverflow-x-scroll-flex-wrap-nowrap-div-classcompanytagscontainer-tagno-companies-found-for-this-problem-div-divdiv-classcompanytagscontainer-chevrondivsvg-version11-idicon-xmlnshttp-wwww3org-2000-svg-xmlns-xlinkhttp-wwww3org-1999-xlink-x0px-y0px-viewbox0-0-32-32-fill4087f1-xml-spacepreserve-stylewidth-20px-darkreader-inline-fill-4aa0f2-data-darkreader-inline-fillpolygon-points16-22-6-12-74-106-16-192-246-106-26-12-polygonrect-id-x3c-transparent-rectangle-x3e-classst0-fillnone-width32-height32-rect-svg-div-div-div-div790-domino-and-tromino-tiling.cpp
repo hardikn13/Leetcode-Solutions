8319804c0304815d862f7dfabbd9891339ce5a38
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2024-06-24 11:44:09 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (29.36%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div790-domino-and-tromino-tiling/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div790-domino-and-tromino-tiling.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div790-domino-and-tromino-tiling/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div790-domino-and-tromino-tiling.cpp
@@ -2,21 +2,51 @@ class Solution {
 public:
     
     int mod = 1e9 + 7;
-    int f(int n, vector<int> &dp)
-    {
+    int numTilings(int n) {
+        vector<int> dp(n + 1, 0);
+        
         if(n == 1 || n == 2)
             return n;
         else if(n == 3)
             return 5;
         
-        if(dp[n] != -1)
-            return dp[n];
-            
-        return dp[n] = ((2 * (f(n - 1, dp) % mod)) % mod + (f(n - 3, dp) % mod)) % mod;
-    }
-    
-    int numTilings(int n) {
-        vector<int> dp(n + 1, -1);
-        return f(n, dp);
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 5;
+        
+        for(int i = 4; i <= n; i++)
+            dp[i] = ((2 * (dp[i - 1] % mod)) % mod + (dp[i - 3] % mod)) % mod;
+        
+        return dp[n];
     }
 };
+
+
+
+
+
+// MEMOIZATION
+// -----------
+
+// class Solution {
+// public:
+    
+//     int mod = 1e9 + 7;
+//     int f(int n, vector<int> &dp)
+//     {
+//         if(n == 1 || n == 2)
+//             return n;
+//         else if(n == 3)
+//             return 5;
+        
+//         if(dp[n] != -1)
+//             return dp[n];
+            
+//         return dp[n] = ((2 * (f(n - 1, dp) % mod)) % mod + (f(n - 3, dp) % mod)) % mod;
+//     }
+    
+//     int numTilings(int n) {
+//         vector<int> dp(n + 1, -1);
+//         return f(n, dp);
+//     }
+// };
