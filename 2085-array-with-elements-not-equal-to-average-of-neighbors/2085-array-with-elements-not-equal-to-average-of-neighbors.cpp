af0c08ff722fc8e6ec8bc151c62f0e35a36a045f
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2024-08-20 18:42:34 +0530

    Time: 131 ms (83.48%), Space: 126.2 MB (69.36%) - LeetHub

---
+++ 2085-array-with-elements-not-equal-to-average-of-neighbors/2085-array-with-elements-not-equal-to-average-of-neighbors.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    // SAME AS WIGGLE SORT
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i < n - 1; i++)
+        {
+            if(i % 2 == 0)
+            {
+                if(nums[i] >= nums[i + 1])
+                    swap(nums[i], nums[i + 1]);
+            }
+            else
+            {
+                if(nums[i] <= nums[i + 1])
+                    swap(nums[i], nums[i + 1]);
+            }
+        }
+
+        return nums;
+    }
+};
