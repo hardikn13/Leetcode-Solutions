a60945bd0b79d0c0748514bde77cd2b8839fba07
Hardik Nahta <106538864+hardikn13@users.noreply.github.com>
2024-10-02 09:00:13 +0530

    Time: 107 ms (21.61%), Space: 49.9 MB (18.82%) - LeetHub

---
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        set<int> st(arr.begin(), arr.end());
+        unordered_map<int, int> mp;
+        int rank = 1;
+        for(auto &it: st)
+            mp[it] = rank++;
+        for(auto &it: arr)
+            it = mp[it];
+        
+        return arr;
+    }
+};
